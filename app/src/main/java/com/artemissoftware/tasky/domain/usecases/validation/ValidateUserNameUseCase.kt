7b2effdd5085036f8671ee86aa384d4941a5f3b7
gustavo <gustavo.isaac@gmail.com>
2023-02-16 16:07:25 +0000

    ValidateUserNameUseCase

---
+++ app/src/main/java/com/artemissoftware/tasky/domain/usecases/validation/ValidateUserNameUseCase.kt
@@ -0,0 +1,18 @@
+package com.artemissoftware.tasky.domain.usecases.validation
+
+class ValidateUserNameUseCase constructor(){
+
+    operator fun invoke(name: String): Boolean  {
+
+        if (name.isEmpty() || name.isBlank()) {
+            return false
+        }
+
+        return USER_NAME_REGEX.toRegex().matches(name)
+    }
+
+    companion object{
+
+        private const val USER_NAME_REGEX = "^[A-Za-z0-9\\s\\S]{4,50}\$" // TODO : min and max from build config
+    }
+}
