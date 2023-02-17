9736a570af0fb56304fea5c9e56ed1486ffb7ed2
gustavo <gustavo.isaac@gmail.com>
2023-02-17 11:05:52 +0000

    EventRadioButton

---
+++ app/src/main/java/com/artemissoftware/tasky/agenda/composables/EventRadioButton.kt
@@ -0,0 +1,54 @@
+package com.artemissoftware.tasky.agenda.composables
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.Icon
+import androidx.compose.material.IconToggleButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.artemissoftware.tasky.R
+
+@Composable
+fun EventRadioButton(
+    modifier: Modifier = Modifier,
+    selected: Boolean = false,
+    onCheckedChange: (Boolean) -> Unit,
+) {
+
+    IconToggleButton(
+        checked = selected,
+        onCheckedChange = onCheckedChange,
+        modifier = modifier.size(24.dp)
+    ) {
+        Icon(
+            painter = painterResource(
+                if (selected) {
+                    R.drawable.ic_launcher_foreground
+                } else {
+                    R.drawable.ic_launcher_background
+                }
+            ),
+            contentDescription = null,
+            tint = MaterialTheme.colors.primary
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun EventRadioButtonPreview() {
+    EventRadioButton(
+        modifier = Modifier,
+        onCheckedChange = {}
+    )
+}
