31b077bb9ab9d91599937fc4d925ff712d58d6cf
gustavo <gustavo.isaac@gmail.com>
2023-02-24 19:10:08 +0000

    TYSurface

--- core-ui/src/main/java/com/artemissoftware/core_ui/composables/scaffold/TYScaffold.kt
+++ core-ui/src/main/java/com/artemissoftware/core_ui/composables/scaffold/TYScaffold.kt
@@ -1,26 +1,29 @@
 package com.artemissoftware.core_ui.composables.scaffold
 
 import androidx.annotation.RawRes
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.artemissoftware.core_ui.R
+import com.artemissoftware.core_ui.composables.TYSurface
 import com.artemissoftware.core_ui.composables.dialog.TYDialog
 import com.artemissoftware.core_ui.composables.dialog.TYDialogOptions
 import com.artemissoftware.core_ui.composables.dialog.TYDialogType
 import com.artemissoftware.core_ui.composables.loading.TYLoading
 import com.artemissoftware.core_ui.composables.topbar.TYToolBarAction
 import com.artemissoftware.core_ui.composables.topbar.TYTopBar
+import com.artemissoftware.core_ui.theme.White
 
 @Composable
 fun TYScaffold(
     modifier: Modifier = Modifier,
+    backgroundColor: Color = White,
     isLoading: Boolean = false,
     @RawRes loadingLottieId: Int = R.raw.loading_lottie,
     tyScaffoldState: TYScaffoldState? = null,
@@ -35,6 +38,7 @@ fun TYScaffold(
 
 
         Scaffold(
+            backgroundColor = backgroundColor,
             modifier = Modifier
                 .fillMaxSize(),
             topBar = {
@@ -132,6 +136,34 @@ private fun FGScaffold_4_Preview() {
             Column(modifier = Modifier.fillMaxSize()) {
                 Text(text = "Text")
             }
+
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FGScaffold_5_Preview() {
+
+    TYScaffold(
+        topBar = {
+            TYTopBar(
+                title = "Top bar title",
+                toolbarActions = { color->
+                    TYToolBarAction(iconId = R.drawable.ic_visibility, tint = color)
+                }
+            )
+        },
+        content = {
+
+            TYSurface(
+                modifier = Modifier.padding(top = 32.dp),
+                content = {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Text("Hello World", modifier = Modifier.padding(8.dp))
+                    }
+                }
+            )
 
         }
     )
