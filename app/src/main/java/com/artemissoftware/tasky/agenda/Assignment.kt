eba4a1016f3239f66ca8e259c452cb61d08c5f47
gustavo <gustavo.isaac@gmail.com>
2023-02-27 19:27:36 +0000

    Assignment class fix

--- app/src/main/java/com/artemissoftware/tasky/agenda/Assignment.kt
+++ app/src/main/java/com/artemissoftware/tasky/agenda/Assignment.kt
@@ -1,12 +1,15 @@
 package com.artemissoftware.tasky.agenda
 
+import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.Color
+import com.artemissoftware.tasky.R
 
-sealed class Assignment(
-    color: Color,
-    borderColor: Color
+sealed class Assignment( // TODO: mudar cores com as da core
+    @StringRes val title: Int,
+    val color: Color,
+    val borderColor: Color
 ){
-    data class Reminder(val color: Color, val borderColor: Color): Assignment(color = color, borderColor = borderColor)
-    data class Task(val color: Color): Assignment(color = color, borderColor = color)
-    data class Event(val color: Color): Assignment(color = color, borderColor = color)
+    class Reminder(): Assignment(color = Color.Red, borderColor = Color.Green, title = R.string.reminder)
+    class Task(color: Color = Color.Blue): Assignment(color = color, borderColor = color, title = R.string.task)
+    class Event(color: Color = Color.Magenta): Assignment(color = Color.Red, borderColor = Color.Green, title = R.string.event)
 }
