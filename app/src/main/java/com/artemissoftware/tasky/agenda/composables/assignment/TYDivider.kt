49fb1eb2bf74b90e64eae8f9977b3dfe1e682ee9
gustavo <gustavo.isaac@gmail.com>
2023-02-27 18:57:26 +0000

    TYDivider

---
+++ app/src/main/java/com/artemissoftware/tasky/agenda/composables/assignment/TYDivider.kt
@@ -0,0 +1,26 @@
+package com.artemissoftware.tasky.agenda.composables.assignment
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TYDivider( // TODO: mudar a cor e por no core UI
+    modifier: Modifier = Modifier,
+    color: Color = Color.Red
+) {
+    Divider(
+        modifier = modifier,
+        color = Color.Green
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TYDividerPreview() {
+    TYDivider(modifier = Modifier.fillMaxWidth())
+
+}
