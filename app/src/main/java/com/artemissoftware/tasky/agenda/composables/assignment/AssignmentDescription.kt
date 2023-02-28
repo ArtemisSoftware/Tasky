d9246e00ccec6649d1dc8a503be1f16aefc0f14f
gustavo <gustavo.isaac@gmail.com>
2023-02-28 09:44:14 +0000

    AssignmentDescription

--- app/src/main/java/com/artemissoftware/tasky/agenda/composables/assignment/AssignmentDescription.kt
+++ app/src/main/java/com/artemissoftware/tasky/agenda/composables/assignment/AssignmentDescription.kt
@@ -1,63 +1,36 @@
 package com.artemissoftware.tasky.agenda.composables.assignment
 
-import android.view.animation.BounceInterpolator
-import androidx.compose.animation.*
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.artemissoftware.tasky.R
-import com.artemissoftware.tasky.agenda.Assignment
 import com.artemissoftware.tasky.agenda.composables.TYIcon
-import com.artemissoftware.tasky.agenda.composables.TYSquare
-import com.artemissoftware.tasky.agenda.composables.WeekDay
 import com.artemissoftware.tasky.util.VisibilityTransitions
 
 @Composable
-fun AssignmentHeader( // TODO: mudar os text para TYtext e as fontes
-    assignment: Assignment,
-    title: String,
+fun AssignmentDescription( // TODO: mudar os text para TYtext e as fontes
+    description: String,
     modifier: Modifier = Modifier,
     isEditing: Boolean = false
 ) {
 
     Column(
-        modifier = Modifier.fillMaxWidth()
+        modifier = modifier.fillMaxWidth()
     ) {
 
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
 
-            TYSquare(
-                size = 20.dp,
-                color = assignment.color,
-                borderColor = assignment.borderColor
-            )
-            Spacer(modifier = Modifier.width(12.dp))
-            Text(text = stringResource(id = assignment.title))
-        }
-
-        Row(
-            modifier = Modifier.padding(top = 32.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-
             Column(
                 modifier = Modifier.weight(0.9F)
             ) {
-                Row {
-                    TYIcon(icon = R.drawable.ic_launcher_foreground)
-                    Spacer(modifier = Modifier.width(12.dp))
-                    Text(text = title)
-                }
+                Text(text = description)
             }
 
             Column(
@@ -88,9 +61,9 @@ fun AssignmentHeader( // TODO: mudar os text para TYtext e as fontes
 @Composable
 private fun AssignmentHeaderPreview() {
     Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-        AssignmentHeader(assignment = Assignment.Reminder(), title = "First title")
-        AssignmentHeader(assignment = Assignment.Task(), isEditing = true, title = "Second title")
-        AssignmentHeader(assignment = Assignment.Event(), isEditing = true, title = "Third title")
+        AssignmentDescription(description = "First description")
+        AssignmentDescription(isEditing = true, description = "Second description")
+        AssignmentDescription(isEditing = true, description = "Second description of a really long one to prove that size is important for the space available")
     }
 
 }
