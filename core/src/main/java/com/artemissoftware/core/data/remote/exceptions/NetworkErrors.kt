56ce2bab88be6dbe46650be18dd0adfb358ac9d7
gustavo <gustavo.isaac@gmail.com>
2023-03-02 10:01:35 +0000

    UiText

--- core/src/main/java/com/artemissoftware/core/data/remote/exceptions/NetworkErrors.kt
+++ core/src/main/java/com/artemissoftware/core/data/remote/exceptions/NetworkErrors.kt
@@ -1,8 +1,9 @@
 package com.artemissoftware.core.data.remote.exceptions
 
+import com.artemissoftware.core.R
+
 object NetworkErrors {
 
-    val UNKNOWN_HOST = 1 to "UnknownHostException"
-    val UNKNOWN_API = 2 to "An error has occurred"
-    val GENERIC_API_ERROR = 3 to "An error has occurred"
+    val UNKNOWN_HOST = 1 to R.string.unknown_host
+    val GENERIC_API_ERROR = 2 to R.string.an_error_occorred
 }
