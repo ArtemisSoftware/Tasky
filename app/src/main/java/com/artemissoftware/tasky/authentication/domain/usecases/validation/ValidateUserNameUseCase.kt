0116988931fa6ad35667fd7f1ba88c8bda499fad
gustavo <gustavo.isaac@gmail.com>
2023-03-02 13:46:39 +0000

    ValidateUserNameUseCaseTest

--- app/src/main/java/com/artemissoftware/tasky/authentication/domain/usecases/validation/ValidateUserNameUseCase.kt
+++ app/src/main/java/com/artemissoftware/tasky/authentication/domain/usecases/validation/ValidateUserNameUseCase.kt
@@ -1,5 +1,7 @@
 package com.artemissoftware.tasky.authentication.domain.usecases.validation
 
+import com.artemissoftware.tasky.BuildConfig.*
+
 class ValidateUserNameUseCase {
 
     operator fun invoke(name: String): Boolean  {
@@ -7,12 +9,11 @@ class ValidateUserNameUseCase {
         if (name.isEmpty() || name.isBlank()) {
             return false
         }
-
         return USER_NAME_REGEX.toRegex().matches(name)
     }
 
     companion object{
 
-        private const val USER_NAME_REGEX = "^[A-Za-z0-9\\s\\S]{4,50}\$" // TODO : min and max from build config
+        private const val USER_NAME_REGEX = "^[A-Za-z0-9\\s\\S]{$MIN_CHARACTERS_FOR_NAME,$MAX_CHARACTERS_FOR_NAME}\$" // TODO : min and max from build config
     }
 }
---
+++ app/src/test/java/com/artemissoftware/tasky/authentication/domain/usecases/validation/ValidateUserNameUseCaseTest.kt
@@ -0,0 +1,52 @@
+package com.artemissoftware.tasky.authentication.domain.usecases.validation
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class ValidateUserNameUseCaseTest {
+
+
+    private lateinit var validateUserNameUseCase: ValidateUserNameUseCase
+
+    @Before
+    fun setUp() {
+        validateUserNameUseCase = ValidateUserNameUseCase()
+    }
+
+    @Test
+    fun `enter valid name get true confirmation`() {
+
+        val name = "Bruce Wayne"
+        Assert.assertTrue(validateUserNameUseCase.invoke(name = name))
+    }
+
+
+    @Test
+    fun `enter a name with less characters than mandatory get false confirmation`() {
+
+        val name = "Ana"
+        Assert.assertFalse(validateUserNameUseCase.invoke(name = name))
+    }
+
+    @Test
+    fun `enter a name with more characters than mandatory get false confirmation`() {
+
+        val name = "Bruce Wayne is the batman and no one knows about it. Have to tell this to the joker"
+        Assert.assertFalse(validateUserNameUseCase.invoke(name = name))
+    }
+
+    @Test
+    fun `enter blank name get false confirmation`() {
+
+        val name = "   "
+        Assert.assertFalse(validateUserNameUseCase.invoke(name = name))
+    }
+
+    @Test
+    fun `enter empty name get false confirmation`() {
+
+        val name = ""
+        Assert.assertFalse(validateUserNameUseCase.invoke(name = name))
+    }
+}
