102d6dbea4aa334b535d88136ae9d716cbafa9ae
gustavo <gustavo.isaac@gmail.com>
2023-03-02 11:34:59 +0000

    TaskyNetworkError

--- core/src/main/java/com/artemissoftware/core/data/remote/exceptions/NetworkErrors.kt
+++
@@ -1,9 +0,0 @@
-package com.artemissoftware.core.data.remote.exceptions
-
-import com.artemissoftware.core.R
-
-object NetworkErrors {
-
-    val UNKNOWN_HOST = 1 to R.string.unknown_host
-    val GENERIC_API_ERROR = 2 to R.string.an_error_occorred
-}
