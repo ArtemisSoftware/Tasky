18b012cdb170ae1fb17adf5e6552cdc83ea7ff6c
gustavo <gustavo.isaac@gmail.com>
2023-03-06 16:36:00 +0000

    PhotoGallery - part 2

--- app/src/main/java/com/artemissoftware/tasky/agenda/presentation/dashboard/composables/PhotoGallery.kt
+++ app/src/main/java/com/artemissoftware/tasky/agenda/presentation/dashboard/composables/PhotoGallery.kt
@@ -1,5 +1,6 @@
 package com.artemissoftware.tasky.agenda.presentation.dashboard.composables
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -9,6 +10,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,23 +22,27 @@ import com.artemissoftware.core.presentation.theme.Light2
 import com.artemissoftware.core.presentation.theme.LightBlue
 import com.artemissoftware.tasky.R
 import com.artemissoftware.tasky.agenda.domain.models.Photo
+import com.artemissoftware.tasky.util.VisibilityTransitions
 
 @Composable
 fun PhotoGallery(
     photos: List<Photo>,
     onAddPhotoClick: ()->Unit,
     modifier: Modifier = Modifier,
+    backgroundColor: Color = Light2,
     isEditing: Boolean = false
 ) {
 
+    val display = Modifier.padding(vertical = 20.dp).then(modifier)
+    val placeHolder = Modifier.clickable { onAddPhotoClick() }.then(modifier)
+
+    val galleryModifier = Modifier
+        .background(color = backgroundColor)
+        .then(if (photos.isEmpty()) placeHolder  else display)
+
     Row(
-        modifier = modifier
-            .clickable {
-                if (photos.isEmpty()) onAddPhotoClick()
-            }
-            .background(color = Light2),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
+        modifier = galleryModifier,
+        verticalAlignment = Alignment.CenterVertically
     ) {
 
         if(photos.isEmpty()){
@@ -44,7 +50,6 @@ fun PhotoGallery(
         }
         else{
             PhotoGalleryDisplay(
-                modifier = Modifier.fillMaxWidth(),
                 photos = photos,
                 isEditing = isEditing
             )
@@ -57,7 +62,9 @@ fun PhotoGallery(
 @Preview(showBackground = true)
 @Composable
 private fun PhotoGalleryPreview() {
-    Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
         PhotoGallery(
             modifier = Modifier
                 .fillMaxWidth()
@@ -86,6 +93,7 @@ private fun PhotoPlaceHolder(
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
     ){
         TaskyIcon(
             color = Gray,
@@ -109,6 +117,10 @@ private fun PhotoPlaceHolderPreview() {
     )
 }
 
+
+
+
+
 @Composable
 private fun PhotoGalleryDisplay(
     photos: List<Photo>,
@@ -116,18 +128,19 @@ private fun PhotoGalleryDisplay(
     isEditing: Boolean = false
 ) {
 
-    Column(modifier = modifier) {
+    Column(
+        modifier = modifier
+
+    ) {
 
         TaskyText(
             style = MaterialTheme.typography.h5,
             text = stringResource(id = R.string.photos)
         )
 
-
         LazyRow(
             modifier = Modifier
-                .padding(top = 20.dp)
-                .fillMaxHeight(),
+                .padding(top = 20.dp),
             horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             items(
@@ -138,6 +151,25 @@ private fun PhotoGalleryDisplay(
                 itemContent = { item ->
 
                     TaskySquareIcon(
+                        url = item.url,
+                        borderWidth = 2.dp,
+                        iconColor = LightBlue,
+                        borderColor = LightBlue,
+                        padding = 8.dp,
+                        size = 60.dp
+                    )
+                }
+            )
+
+            item {
+
+                AnimatedVisibility(
+                    visible = isEditing,
+                    enter = VisibilityTransitions.enterEdition(),
+                    exit = VisibilityTransitions.exitEdition()
+                ) {
+
+                    TaskySquareIcon(
                         icon = R.drawable.ic_add,
                         borderWidth = 2.dp,
                         iconColor = LightBlue,
@@ -145,9 +177,8 @@ private fun PhotoGalleryDisplay(
                         padding = 20.dp,
                         size = 60.dp
                     )
-
                 }
-            )
+            }
         }
 
     }
