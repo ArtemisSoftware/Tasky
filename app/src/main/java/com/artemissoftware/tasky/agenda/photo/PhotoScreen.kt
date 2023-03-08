a5627592c89b25ac4b3887d1e6ae2ca58f9f5c27
gustavo <gustavo.isaac@gmail.com>
2023-03-08 15:21:54 +0000

    ui refactor

--- app/src/main/java/com/artemissoftware/tasky/agenda/photo/PhotoScreen.kt
+++ app/src/main/java/com/artemissoftware/tasky/agenda/photo/PhotoScreen.kt
@@ -1,13 +1,25 @@
 package com.artemissoftware.tasky.agenda.photo
 
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import com.artemissoftware.core.presentation.composables.icon.TaskySquareIcon
 import com.artemissoftware.core.presentation.composables.scaffold.TaskyScaffold
 import com.artemissoftware.core.presentation.composables.topbar.TaskyToolBarAction
 import com.artemissoftware.core.presentation.composables.topbar.TaskyTopBar
 import com.artemissoftware.core.presentation.theme.Black
+import com.artemissoftware.core.presentation.theme.LightBlue
 import com.artemissoftware.tasky.R
+import com.artemissoftware.core.R as CoreR
 
 @Composable
 fun PhotoScreen(
@@ -27,7 +39,7 @@ fun PhotoScreen(
                 title = stringResource(id = R.string.photo),
                 toolbarActions = { color->
                     TaskyToolBarAction(
-                        iconId = R.drawable.ic_launcher_background,
+                        iconId = R.drawable.ic_trash,
                         tint = color,
                         onClicked = {
                             events(PhotoEvents.DeletePhoto(""))
@@ -38,10 +50,41 @@ fun PhotoScreen(
         },
         content = {
 
+            state.photo?.let {
 
+                val painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(it.url)
+                        .size(Size.ORIGINAL)
+                        .crossfade(800)
+                        .error(CoreR.drawable.ic_tasky_logo)
+                        .placeholder(CoreR.drawable.ic_tasky_logo)
+                        .build()
+                )
 
+                when(painter.state){
 
+                    is AsyncImagePainter.State.Error ->{
+                        TaskySquareIcon(
+                            modifier = Modifier,
+                            size = 32.dp,
+                            borderWidth = 2.dp,
+                            borderColor = LightBlue,
+                            icon = CoreR.drawable.ic_tasky_logo
+                        )
+                    }
 
+                    is AsyncImagePainter.State.Success ->{
+                        Image(
+                            modifier = Modifier,
+                            painter = painter,
+                            contentDescription = "",
+                            contentScale = ContentScale.Crop,
+                        )
+                    }
+                    else ->{}
+                }
+            }
         }
     )
 }
