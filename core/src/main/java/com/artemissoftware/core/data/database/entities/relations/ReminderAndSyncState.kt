63b53bae1a959b990f28cbf2bc6d5ed3a12c8074
gustavo <gustavo.isaac@gmail.com>
2023-03-14 13:02:55 +0000

    ReminderAndSyncState

---
+++ core/src/main/java/com/artemissoftware/core/data/database/entities/relations/ReminderAndSyncState.kt
@@ -0,0 +1,15 @@
+package com.artemissoftware.core.data.database.entities.relations
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.artemissoftware.core.data.database.entities.ReminderEntity
+import com.artemissoftware.core.data.database.entities.ReminderSyncEntity
+
+data class ReminderAndSyncState (
+    @Embedded val reminder: ReminderEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "id"
+    )
+    val reminderSyncEntity: ReminderSyncEntity
+)
