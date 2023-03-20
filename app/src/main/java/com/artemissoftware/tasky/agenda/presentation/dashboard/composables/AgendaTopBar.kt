6aafdc4e83c27ee4772864e5e3a328326cd1b5c1
gustavo <gustavo.isaac@gmail.com>
2023-03-20 15:54:48 +0000

    Ui updates

--- app/src/main/java/com/artemissoftware/tasky/agenda/presentation/dashboard/composables/AgendaTopBar.kt
+++ app/src/main/java/com/artemissoftware/tasky/agenda/presentation/dashboard/composables/AgendaTopBar.kt
@@ -9,15 +9,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.artemissoftware.core.presentation.composables.TaskyAvatar
 import com.artemissoftware.core.presentation.composables.TaskyDropDown
 import com.artemissoftware.core.presentation.composables.dropdown.TaskyDropDownItem
+import com.artemissoftware.core.presentation.composables.icon.TaskyIcon
+import com.artemissoftware.core.presentation.composables.text.TaskyText
+import com.artemissoftware.core.presentation.theme.Black
 import com.artemissoftware.core.presentation.theme.Green
 import com.artemissoftware.core.presentation.theme.LightBlue
 
@@ -56,10 +63,25 @@ private fun AgendaTopBarPreview() {
             toolbarActionsLeft = {
                 TaskyDropDown<String>(
                     options = listOf("Tasky 1", "Tasky 2", "Tasky 3"),
-                    defaultOption = "priority",
                     onOptionSelected = {},
                     addDivider = true,
                     modifier = Modifier.width(140.dp),
+                    placeHolder = {
+                        TaskyText(
+                            modifier = Modifier
+                                .weight(weight = 8.5f),
+                            text = "defaultOption",
+                        )
+
+                        TaskyIcon(
+                            size = 24.dp,
+                            modifier = Modifier
+                                .alpha(ContentAlpha.medium)
+                                .weight(weight = 1.5f),
+                            icon = Icons.Filled.ArrowDropDown,
+                            color = Black,
+                        )
+                    },
                     menuOption = {
                         TaskyDropDownItem(text = "Tasky")
                     },
--- core/src/main/java/com/artemissoftware/core/presentation/composables/DropDownMenu.kt
+++ core/src/main/java/com/artemissoftware/core/presentation/composables/DropDownMenu.kt
@@ -1,6 +1,5 @@
 package com.artemissoftware.core.presentation.composables
 
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -12,10 +11,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
@@ -28,23 +25,15 @@ import com.artemissoftware.core.presentation.theme.*
 @Composable
 fun <T>TaskyDropDown(
     options: List<T>,
-    defaultOption: String,
+    placeHolder: @Composable RowScope.() -> Unit,
     menuOption: @Composable (T) -> Unit,
     onOptionSelected: (T) -> Unit,
     modifier: Modifier = Modifier,
-    allCaps: Boolean = false,
     addDivider: Boolean = false,
-    textStyle: TextStyle = MaterialTheme.typography.subtitle2,
     height: Dp = 36.dp,
     backgroundColor: Color = White,
-    textColor: Color = LightBlue
 ) {
-
     var expanded by remember { mutableStateOf(false) }
-    val angle: Float by animateFloatAsState(
-        targetValue = if (expanded) 180f else 0f
-    )
-
     var parentSize by remember { mutableStateOf(IntSize.Zero) }
 
     Row(
@@ -55,90 +44,92 @@ fun <T>TaskyDropDown(
             .background(backgroundColor)
             .height(height)
             .clickable { expanded = true },
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-
-        TaskyText(
-            modifier = Modifier
-                .weight(weight = 8.5f),
-            text = defaultOption,
-            allCaps = allCaps,
-            color = textColor,
-            style = textStyle
-        )
-
-        TaskyIcon(
-            size = 24.dp,
-            modifier = Modifier
-                .graphicsLayer {
-                    rotationX = angle
-                }
-                .alpha(ContentAlpha.medium)
-                .weight(weight = 1.5f),
-            icon = Icons.Filled.ArrowDropDown,
-            color = textColor
-        )
+        placeHolder()
     }
-        DropdownMenu(
-            modifier = Modifier
-                .width(with(LocalDensity.current) { parentSize.width.toDp() }),
-            expanded = expanded,
-            onDismissRequest = { expanded = false }
-        ) {
-            options.forEachIndexed { index, item ->
+    DropdownMenu(
+        modifier = Modifier
+            .width(with(LocalDensity.current) { parentSize.width.toDp() }),
+        expanded = expanded,
+        onDismissRequest = { expanded = false },
+    ) {
+        options.forEachIndexed { index, item ->
 
-                val isLast = (index == options.size -1)
+            val isLast = (index == options.size - 1)
 
-                DropdownMenuItem(
-                    onClick = {
-                        expanded = false
-                        onOptionSelected(item)
-                    }
-                ) {
-                    menuOption(item)
-                }
-
-                if(addDivider && !isLast) {
-
-                    Divider(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(9.dp)
-                            .padding(top = 8.dp),
-                        color = Light
-                    )
-                }
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onOptionSelected(item)
+                },
+            ) {
+                menuOption(item)
             }
 
+            if (addDivider && !isLast) {
+                Divider(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(9.dp)
+                        .padding(top = 8.dp),
+                    color = Light,
+                )
+            }
+        }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 private fun TaskyDropDownPreview() {
-
     Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+        verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
-
         TaskyDropDown<String>(
             options = listOf("Tasky 1", "Tasky 2", "Tasky 3"),
-            defaultOption = "priority",
             onOptionSelected = {},
+            placeHolder = {
+                TaskyText(
+                    modifier = Modifier
+                        .weight(weight = 8.5f),
+                    text = "defaultOption",
+                )
+
+                TaskyIcon(
+                    size = 24.dp,
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium)
+                        .weight(weight = 1.5f),
+                    icon = Icons.Filled.ArrowDropDown,
+                    color = Black,
+                )
+            },
             addDivider = true,
             modifier = Modifier.width(140.dp),
             menuOption = {
-
                 TaskyDropDownItem(text = "Tasky")
-
-            }
+            },
         )
 
         TaskyDropDown<String>(
             options = listOf("Tasky 1", "Tasky 2"),
-            defaultOption = "priority2",
-            textColor = Green,
+            placeHolder = {
+                TaskyText(
+                    modifier = Modifier
+                        .weight(weight = 8.5f),
+                    text = "defaultOption",
+                )
+
+                TaskyIcon(
+                    size = 24.dp,
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium)
+                        .weight(weight = 1.5f),
+                    icon = Icons.Filled.ArrowDropDown,
+                    color = Black,
+                )
+            },
             backgroundColor = Black,
             onOptionSelected = {},
             modifier = Modifier.fillMaxWidth(),
@@ -146,11 +137,9 @@ private fun TaskyDropDownPreview() {
                 Text(
                     modifier = Modifier,
                     text = it,
-                    style = MaterialTheme.typography.subtitle2
+                    style = MaterialTheme.typography.subtitle2,
                 )
-            }
+            },
         )
     }
-
 }
-
