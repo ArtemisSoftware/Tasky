fd122f2415ddd2258e5c742f930bb99a151efbf5
gustavo <gustavo.isaac@gmail.com>
2023-03-20 16:32:27 +0000

    TaskSyncEntity

---
+++ core/src/main/java/com/artemissoftware/core/data/database/entities/TaskSyncEntity.kt
@@ -0,0 +1,12 @@
+package com.artemissoftware.core.data.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.artemissoftware.core.domain.SyncType
+
+@Entity
+data class TaskSyncEntity(
+    @PrimaryKey
+    val id: String,
+    val syncType: SyncType,
+)
