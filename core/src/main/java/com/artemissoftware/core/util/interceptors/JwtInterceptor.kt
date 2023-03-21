2c30a5d0e79d0e0576fe2ee7632f727dea4a73a1
gustavo <gustavo.isaac@gmail.com>
2023-03-21 19:11:14 +0000

    JwtInterceptor

--- core/src/main/java/com/artemissoftware/core/util/interceptors/JwtInterceptor.kt
+++ core/src/main/java/com/artemissoftware/core/util/interceptors/JwtInterceptor.kt
@@ -1,26 +1,43 @@
 package com.artemissoftware.core.util.interceptors
 
 import com.artemissoftware.core.domain.usecase.GetUserUseCase
+import com.artemissoftware.core.util.annotations.NeedsTokenHeaderRequest
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
+import retrofit2.Invocation
 
 // TODO: add @inject anotation on constructor (when hilt is implemented) to inject usecase
 class JwtInterceptor constructor(private val getUserUseCase: GetUserUseCase) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
+        val original = chain.request()
+
+        original.tag(Invocation::class.java)?.let { invocation ->
+            if (invocation.method().isAnnotationPresent(NeedsTokenHeaderRequest::class.java)) {
+                val request: Request = original.newBuilder()
+                    .header(HEADER_AUTHORIZATION, "$BEARER ${getToken()}")
+                    .method(original.method, original.body)
+                    .build()
+
+                return chain.proceed(request)
+            } else {
+                return chain.proceed(original)
+            }
+        } ?: run {
+            return chain.proceed(original)
+        }
+    }
+
+    private fun getToken(): String {
         var token = ""
         runBlocking {
             token = getUserUseCase.invoke().first().token
         }
 
-        val request = chain.request().newBuilder()
-
-        if (token.isNotEmpty()) {
-            request.addHeader(HEADER_AUTHORIZATION, "$BEARER $token")
-        }
-        return chain.proceed(request.build())
+        return token
     }
 
     companion object {
