e28bc3864743c59a501246c41a4cd3fa63933876
gustavo <gustavo.isaac@gmail.com>
2023-03-21 19:10:55 +0000

    Token annotation

--- app/src/main/java/com/artemissoftware/tasky/authentication/data/remote/TaskyAuthenticationApi.kt
+++ app/src/main/java/com/artemissoftware/tasky/authentication/data/remote/TaskyAuthenticationApi.kt
@@ -1,5 +1,6 @@
 package com.artemissoftware.tasky.authentication.data.remote
 
+import com.artemissoftware.core.util.annotations.NeedsTokenHeaderRequest
 import com.artemissoftware.core.util.annotations.NoApiKeyHeaderRequest
 import com.artemissoftware.tasky.authentication.data.remote.dto.LoginBodyDto
 import com.artemissoftware.tasky.authentication.data.remote.dto.LoginResponseDto
@@ -19,6 +20,7 @@ interface TaskyAuthenticationApi {
     @POST("login")
     suspend fun loginUser(@Body loginBodyDto: LoginBodyDto): LoginResponseDto
 
+    @NeedsTokenHeaderRequest
     @GET("authenticate")
     suspend fun authenticate(): ResponseBody
 
--- core/src/main/java/com/artemissoftware/core/util/annotations/Annotations.kt
+++ core/src/main/java/com/artemissoftware/core/util/annotations/Annotations.kt
@@ -3,3 +3,7 @@ package com.artemissoftware.core.util.annotations
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class NoApiKeyHeaderRequest
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class NeedsTokenHeaderRequest
