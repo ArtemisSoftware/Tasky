a753e8326b85a78fc0eae53a04fc8da7b85b3ae7
gustavo <gustavo.isaac@gmail.com>
2023-03-22 16:37:00 +0000

    compose-destinations dependencies

--- core/src/main/java/com/artemissoftware/core/presentation/navigation/models/CustomNavigationArgument.kt
+++
@@ -1,9 +0,0 @@
-package com.artemissoftware.core.presentation.navigation.models
-
-import androidx.navigation.NavType
-
-data class CustomNavigationArgument(
-    val key: String,
-    val type: NavType<*> = NavType.StringType,
-    val nullable: Boolean = true
-)
