4f5c7a31bbfec62e339f898a19ddbf5e9d6bd81f
gustavo <gustavo.isaac@gmail.com>
2023-03-22 15:46:33 +0000

    Integrating di on the app

--- app/src/main/java/com/artemissoftware/tasky/authentication/domain/usecases/LoginUseCase.kt
+++ app/src/main/java/com/artemissoftware/tasky/authentication/domain/usecases/LoginUseCase.kt
@@ -1,22 +1,22 @@
 package com.artemissoftware.tasky.authentication.domain.usecases
 
-import com.artemissoftware.core.domain.models.Resource
-import com.artemissoftware.core.domain.models.DataResponse
-import com.artemissoftware.core.domain.repositories.UserStoreRepository
 import com.artemissoftware.core.domain.AuthenticationException
 import com.artemissoftware.core.domain.ValidationException
+import com.artemissoftware.core.domain.models.DataResponse
+import com.artemissoftware.core.domain.models.Resource
+import com.artemissoftware.core.domain.repositories.UserStoreRepository
 import com.artemissoftware.tasky.authentication.domain.repositories.AuthenticationRepository
+import javax.inject.Inject
 
-class LoginUseCase constructor(
+class LoginUseCase @Inject constructor(
     private val userStoreRepository: UserStoreRepository,
-    private val authenticationRepository: AuthenticationRepository
-){
+    private val authenticationRepository: AuthenticationRepository,
+) {
 
     suspend operator fun invoke(email: String, password: String): Resource<Boolean> {
-
         val result = authenticationRepository.loginUser(email = email, password = password)
 
-        return when(result){
+        return when (result) {
             is DataResponse.Error -> {
                 val exception = result.exception?.description?.let { ValidationException.DataError(it) } ?: AuthenticationException.LoginError
                 Resource.Error(exception)
--- app/src/main/java/com/artemissoftware/tasky/authentication/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/artemissoftware/tasky/authentication/presentation/login/LoginScreen.kt
@@ -1,8 +1,12 @@
 package com.artemissoftware.tasky.authentication.presentation.login
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -11,29 +15,25 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.artemissoftware.core.presentation.composables.TaskyContentSurface
 import com.artemissoftware.core.presentation.composables.button.TaskyTextButton
 import com.artemissoftware.core.presentation.composables.scaffold.TaskyScaffold
-import com.artemissoftware.core.presentation.composables.scaffold.TaskyScaffoldState
 import com.artemissoftware.core.presentation.composables.text.TaskyText
-import com.artemissoftware.core.presentation.events.UiEvent
 import com.artemissoftware.core.presentation.theme.Black
 import com.artemissoftware.core.presentation.theme.Link
 import com.artemissoftware.core.presentation.theme.White
 import com.artemissoftware.tasky.R
 import com.artemissoftware.tasky.authentication.presentation.login.composables.LoginForm
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
 @Composable
-fun LoginScreen(viewModel: LoginViewModel /* TODO : init viewmodel with Hilt when dependency is included on the project */) {
-
+fun LoginScreen(viewModel: LoginViewModel = hiltViewModel()) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
     LoginScreenContent(
         state = state,
-        events = viewModel::onTriggerEvent
+        events = viewModel::onTriggerEvent,
     )
 
     ManageUIEvents(
@@ -46,29 +46,26 @@ fun LoginScreen(viewModel: LoginViewModel /* TODO : init viewmodel with Hilt whe
     )
 }
 
-
 @Composable
 private fun LoginScreenContent(
     state: LoginState,
-    events: (LoginEvents) -> Unit
+    events: (LoginEvents) -> Unit,
 ) {
     TaskyScaffold(
         isLoading = state.isLoading,
         taskyScaffoldState = state.scaffoldState,
         backgroundColor = Black,
         content = {
-
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 TaskyText(
                     modifier = Modifier
                         .padding(vertical = 48.dp),
                     color = White,
                     style = MaterialTheme.typography.h4,
-                    text = stringResource(id = R.string.welcome_back)
+                    text = stringResource(id = R.string.welcome_back),
                 )
-
 
                 TaskyContentSurface(
                     content = {
@@ -76,16 +73,15 @@ private fun LoginScreenContent(
                             modifier = Modifier
                                 .padding(top = 48.dp)
                                 .padding(bottom = 68.dp)
-                                .padding(horizontal = 16.dp)
-                        ){
-
+                                .padding(horizontal = 16.dp),
+                        ) {
                             LoginForm(
                                 modifier = Modifier.align(Alignment.TopCenter),
                                 email = state.email,
                                 emailValidationStateType = state.emailValidationStateType,
                                 password = state.password,
                                 passwordValidationStateType = state.passwordValidationStateType,
-                                events = events
+                                events = events,
                             )
 
                             TaskyTextButton(
@@ -98,26 +94,22 @@ private fun LoginScreenContent(
                                     append(" ")
                                     withStyle(
                                         style = SpanStyle(
-                                            color = Link
-                                        )
+                                            color = Link,
+                                        ),
                                     ) {
                                         append(stringResource(id = R.string.sign_up))
                                     }
                                 },
                                 onClick = {
                                     events(LoginEvents.SignUp)
-                                }
+                                },
                             )
                         }
-                    }
+                    },
                 )
             }
-
-        }
+        },
     )
-
-
-
 }
 
 @Preview(showBackground = true)
