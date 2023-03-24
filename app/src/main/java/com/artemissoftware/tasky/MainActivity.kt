e481398c97e27059c18f87854631a7529357ce22
gustavo <gustavo.isaac@gmail.com>
2023-03-24 12:59:32 +0000

    Splash logic

--- app/src/main/java/com/artemissoftware/tasky/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/tasky/MainActivity.kt
@@ -3,6 +3,7 @@ package com.artemissoftware.tasky
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,11 +11,19 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.artemissoftware.tasky.ui.theme.TaskyTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepVisibleCondition {
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             TaskyTheme {
                 // A surface container using the 'background' color from the theme
