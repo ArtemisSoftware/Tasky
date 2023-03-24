82a85ea8fd91142662601edbe9edb9d244912119
gustavo <gustavo.isaac@gmail.com>
2023-03-24 20:28:04 +0000

    PhotoEntity

---
+++ core/src/main/java/com/artemissoftware/core/data/database/entities/PhotoEntity.kt
@@ -0,0 +1,22 @@
+package com.artemissoftware.core.data.database.entities
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = EventEntity::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("eventId"),
+            onDelete = ForeignKey.CASCADE,
+        ),
+    ],
+)
+data class PhotoEntity(
+    @PrimaryKey
+    val key: String,
+    val url: String,
+    val eventId: String,
+)
