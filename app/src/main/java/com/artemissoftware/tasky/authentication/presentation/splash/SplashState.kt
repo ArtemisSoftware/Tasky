56bb335a9904b0da8a60a0deeac457f7aed1b25e
gustavo <gustavo.isaac@gmail.com>
2023-03-24 20:06:54 +0000

    Remove splash package

--- app/src/main/java/com/artemissoftware/tasky/authentication/presentation/splash/SplashState.kt
+++
@@ -1,7 +0,0 @@
-package com.artemissoftware.tasky.authentication.presentation.splash
-
-import com.artemissoftware.core.presentation.composables.scaffold.TaskyScaffoldState
-
-data class SplashState(
-    val scaffoldState: TaskyScaffoldState = TaskyScaffoldState(),
-)
