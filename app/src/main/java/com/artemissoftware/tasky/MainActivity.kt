21018d53805eb3593658d993f2fe4f3e22e8e263
gustavo <gustavo.isaac@gmail.com>
2023-03-24 20:05:29 +0000

    MainActivity update

--- app/src/main/java/com/artemissoftware/tasky/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/tasky/MainActivity.kt
@@ -4,20 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.artemissoftware.tasky.authentication.presentation.NavGraphs
-import com.artemissoftware.tasky.authentication.presentation.destinations.LoginScreenDestination
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.artemissoftware.core.R
+import com.artemissoftware.core.presentation.composables.icon.TaskyIcon
+import com.artemissoftware.core.presentation.composables.scaffold.TaskyScaffold
+import com.artemissoftware.core.presentation.theme.Green
+import com.artemissoftware.core.presentation.theme.White
+import com.artemissoftware.tasky.authentication.presentation.login.ManageUIEvents
 import com.artemissoftware.tasky.ui.theme.TaskyTheme
 import dagger.hilt.android.AndroidEntryPoint
-import com.ramcosta.composedestinations.DestinationsNavHost
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -27,7 +33,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
             setKeepVisibleCondition {
-                viewModel.isLoading.value
+                viewModel.state.value.isLoading
             }
         }
         setContent {
@@ -37,16 +43,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-//                    if (!viewModel.isLoading.collectAsState().value) {
-//                        DestinationsNavHost(
-//                            navGraph = NavGraphs.root,
-//                            startRoute = LoginScreenDestination,
-//                        )
-//                    }
+                    MainScreen(viewModel)
                 }
             }
         }
     }
+}
+
+@Composable
+private fun MainScreen(viewModel: MainViewModel) {
+    val state = viewModel.state.collectAsStateWithLifecycle().value
+
+    TaskyScaffold(
+        taskyScaffoldState = state.scaffoldState,
+        backgroundColor = Green,
+        content = {
+            Box(modifier = Modifier.fillMaxSize()) {
+                TaskyIcon(
+                    modifier = Modifier.align(Alignment.Center),
+                    color = White,
+                    size = 140.dp,
+                    icon = R.drawable.ic_tasky_logo,
+                )
+            }
+        },
+    )
+
+    ManageUIEvents(
+        uiEvent = viewModel.uiEvent,
+        showDialog = {
+            state.scaffoldState.showDialog(it)
+        },
+    )
 }
 
 @Composable
