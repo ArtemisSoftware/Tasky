b6145e4c436a6b2faf5ba80e6852af6f43735d02
gustavo <gustavo.isaac@gmail.com>
2023-03-24 21:13:53 +0000

    Update TaskyDatabase

--- core/src/main/java/com/artemissoftware/core/data/database/TaskyDatabase.kt
+++ core/src/main/java/com/artemissoftware/core/data/database/TaskyDatabase.kt
@@ -2,10 +2,15 @@ package com.artemissoftware.core.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.artemissoftware.core.data.database.dao.EventDao
 import com.artemissoftware.core.data.database.dao.NotificationWarningDao
 import com.artemissoftware.core.data.database.dao.ReminderDao
 import com.artemissoftware.core.data.database.dao.TaskDao
+import com.artemissoftware.core.data.database.entities.EventAttendeeEntity
+import com.artemissoftware.core.data.database.entities.EventEntity
+import com.artemissoftware.core.data.database.entities.EventSyncEntity
 import com.artemissoftware.core.data.database.entities.NotificationWarningEntity
+import com.artemissoftware.core.data.database.entities.PhotoEntity
 import com.artemissoftware.core.data.database.entities.ReminderEntity
 import com.artemissoftware.core.data.database.entities.ReminderSyncEntity
 import com.artemissoftware.core.data.database.entities.TaskEntity
@@ -18,6 +23,10 @@ import com.artemissoftware.core.data.database.entities.TaskSyncEntity
         ReminderSyncEntity::class,
         TaskEntity::class,
         TaskSyncEntity::class,
+        EventEntity::class,
+        EventAttendeeEntity::class,
+        PhotoEntity::class,
+        EventSyncEntity::class,
     ],
     version = 1,
 )
@@ -26,4 +35,5 @@ abstract class TaskyDatabase : RoomDatabase() {
     abstract val notificationWarningDao: NotificationWarningDao
     abstract val reminderDao: ReminderDao
     abstract val taskDao: TaskDao
+    abstract val eventDao: EventDao
 }
