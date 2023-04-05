f55da8d8c43c78e7e992668f2f23ec66646d7067
gustavo <gustavo.isaac@gmail.com>
2023-04-05 18:09:24 +0100

    AgendaItemType

---
+++ app/src/main/java/com/artemissoftware/tasky/agenda/domain/models/AgendaItemType.kt
@@ -0,0 +1,21 @@
+package com.artemissoftware.tasky.agenda.domain.models
+
+import com.artemissoftware.core.util.UiText
+import com.artemissoftware.tasky.R
+
+enum class AgendaItemType(val text: UiText) {
+
+    EVENT(UiText.StringResource(R.string.event)),
+    REMINDER(UiText.StringResource(R.string.reminder)),
+    TASK(UiText.StringResource(R.string.task)),
+    ;
+
+    companion object {
+        fun convertAgendaItem(agendaItem: AgendaItem): AgendaItemType {
+            return when (agendaItem) {
+                is AgendaItem.Reminder -> REMINDER
+                is AgendaItem.Task -> TASK
+            }
+        }
+    }
+}
