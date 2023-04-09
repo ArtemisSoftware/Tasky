bb817e2d558cf04c0a269c596d08a9b781f08a04
gustavo <gustavo.isaac@gmail.com>
2023-04-09 11:29:43 +0100

    AlarmBootUpReceiver

---
+++ app/src/main/java/com/artemissoftware/tasky/agenda/data/alarm/AlarmBootUpReceiver.kt
@@ -0,0 +1,35 @@
+package com.artemissoftware.tasky.agenda.data.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.artemissoftware.tasky.agenda.domain.alarm.AlarmScheduler
+import com.artemissoftware.tasky.agenda.domain.usecase.agenda.GetAgendaToUpdateAlarmsUseCase
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class AlarmBootUpReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var getAgendaToUpdateAlarmsUseCase: GetAgendaToUpdateAlarmsUseCase
+
+    @Inject
+    lateinit var alarmScheduler: AlarmScheduler
+
+    private val job = SupervisorJob()
+
+    override fun onReceive(context: Context?, intent: Intent?)  {
+
+        CoroutineScope(job).launch {
+
+            val items = getAgendaToUpdateAlarmsUseCase()
+            items.forEach {
+                alarmScheduler.schedule(it)
+            }
+        }
+    }
+}
