672b98bdbe08eaee7947dea8bae08812eafdcd99
gustavo <gustavo.isaac@gmail.com>
2023-04-14 17:15:22 +0100

    ImageCompressor

---
+++ app/src/main/java/com/artemissoftware/tasky/agenda/data/compressor/ImageCompressorImpl.kt
@@ -0,0 +1,27 @@
+package com.artemissoftware.tasky.agenda.data.compressor
+
+import android.app.Application
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import com.artemissoftware.core.util.constants.ImageSizeConstants.COMPRESSION_QUALITY
+import com.artemissoftware.tasky.agenda.domain.compressor.ImageCompressor
+import java.io.ByteArrayOutputStream
+import javax.inject.Inject
+
+class ImageCompressorImpl @Inject constructor(
+    private val application: Application,
+) : ImageCompressor {
+
+    override suspend fun compressUri(uri: String): ByteArray {
+        val bytes = application.contentResolver.openInputStream(Uri.parse(uri))?.use {
+            it.readBytes()
+        } ?: byteArrayOf()
+
+        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, COMPRESSION_QUALITY, outputStream)
+
+        return outputStream.toByteArray()
+    }
+}
--- core/src/main/java/com/artemissoftware/core/util/constants/ImageSizeConstants.kt
+++ core/src/main/java/com/artemissoftware/core/util/constants/ImageSizeConstants.kt
@@ -3,4 +3,5 @@ package com.artemissoftware.core.util.constants
 object ImageSizeConstants {
 
     const val ONE_MEGA_BYTE = 1048576
+    const val COMPRESSION_QUALITY = 85
 }
