7f12089c472ce596d1fd4fc46851228cccac1862
gustavo <gustavo.isaac@gmail.com>
2023-04-17 16:52:57 +0100

    EventMapper

---
+++ app/src/main/java/com/artemissoftware/tasky/agenda/data/mappers/EventMapper.kt
@@ -0,0 +1,33 @@
+package com.artemissoftware.tasky.agenda.data.mappers
+
+import com.artemissoftware.core.data.database.entities.EventEntity
+import com.artemissoftware.core.data.database.entities.relations.EventAndSyncState
+import com.artemissoftware.core.util.extensions.toLocalDateTime
+import com.artemissoftware.core.util.extensions.toLong
+import com.artemissoftware.tasky.agenda.domain.models.AgendaItem
+
+fun AgendaItem.Event.toEntity(): EventEntity {
+    return EventEntity(
+        title = title,
+        description = this.description ?: "",
+        id = this.id,
+        remindAt = remindAt.toLong(),
+        to = to.toLong(),
+        from = from.toLong(),
+        isUserEventCreator = isUserEventCreator,
+        host = host,
+    )
+}
+
+fun EventAndSyncState.toAgendaItem(): AgendaItem.Event {
+    return AgendaItem.Event(
+        title = this.event.title,
+        description = this.event.description,
+        id = this.event.id,
+        host = this.event.host,
+        remindAt = this.event.remindAt.toLocalDateTime(),
+        to = this.event.to.toLocalDateTime(),
+        from = this.event.from.toLocalDateTime(),
+        attendees = attendees.map { it.toAttendee() },
+    )
+}
