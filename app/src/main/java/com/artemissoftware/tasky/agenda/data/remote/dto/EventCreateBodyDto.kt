f3bf832b3a608924ed14dbfea100d14dda55b4ce
gustavo <gustavo.isaac@gmail.com>
2023-04-19 10:26:29 +0100

    EventCreateBodyDto

---
+++ app/src/main/java/com/artemissoftware/tasky/agenda/data/remote/dto/EventCreateBodyDto.kt
@@ -0,0 +1,20 @@
+package com.artemissoftware.tasky.agenda.data.remote.dto
+
+import com.squareup.moshi.Json
+
+data class EventCreateBodyDto(
+    @field:Json(name = "description")
+    val description: String,
+    @field:Json(name = "from")
+    val from: Long,
+    @field:Json(name = "id")
+    val id: String,
+    @field:Json(name = "remindAt")
+    val remindAt: Long,
+    @field:Json(name = "title")
+    val title: String,
+    @field:Json(name = "to")
+    val to: Long,
+    @field:Json(name = "attendeeIds")
+    val attendeeIds: List<String>,
+)
