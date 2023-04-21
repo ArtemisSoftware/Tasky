3f8ef207e6dbb2d1fe6ee37d97bf4c3de7c01099
gustavo <gustavo.isaac@gmail.com>
2023-04-21 20:03:45 +0100

    Configuration provider

--- app/src/main/java/com/artemissoftware/tasky/TaskyApplication.kt
+++ app/src/main/java/com/artemissoftware/tasky/TaskyApplication.kt
@@ -1,7 +1,17 @@
 package com.artemissoftware.tasky
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class TaskyApplication : Application()
+class TaskyApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() = Configuration.Builder()
+        .setWorkerFactory(workerFactory)
+        .build()
+}
