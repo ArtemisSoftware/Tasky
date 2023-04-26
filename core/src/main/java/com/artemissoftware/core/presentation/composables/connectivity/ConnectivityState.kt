e7e173e4cad3b143b1b4f332ba4ed7f3e5c99ee2
gustavo <gustavo.isaac@gmail.com>
2023-04-26 16:09:03 +0100

    Added optIn

--- core/src/main/java/com/artemissoftware/core/presentation/composables/connectivity/ConnectivityState.kt
+++ core/src/main/java/com/artemissoftware/core/presentation/composables/connectivity/ConnectivityState.kt
@@ -8,12 +8,14 @@ import androidx.compose.ui.platform.LocalContext
 import com.artemissoftware.core.presentation.composables.connectivity.models.TaskyConnectionState
 import com.artemissoftware.core.util.extensions.currentConnectivityState
 import com.artemissoftware.core.util.extensions.observeConnectivityAsFlow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @Composable
 fun currentConnectionState(): TaskyConnectionState = with(LocalContext.current) {
     return remember { currentConnectivityState }
 }
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @Composable
 fun connectivityState(): State<TaskyConnectionState> = with(LocalContext.current) {
     return produceState(initialValue = currentConnectivityState) {
